133770c3bf0159c0df9431d487529a3c5b3a20e5
KevinZhang <zhangkevin34@gmail.com>
2019-10-11 01:46:53 +1100

    block floating up
    block floating up when game start

---
+++ assessment4/Assets/C#/BlockMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockMove : MonoBehaviour
+{
+    private float previousTime;
+    public float fallingTime = 0.7f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            transform.position += new Vector3(+1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            transform.position += new Vector3(0, -1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            transform.position += new Vector3(0, +1, 0);
+        }
+        if (Time.time - previousTime > fallingTime) 
+        {
+            transform.position += new Vector3(0, +1, 0);
+            previousTime = Time.time; 
+        }
+
+    }
+}
