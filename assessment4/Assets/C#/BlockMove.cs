da00758b333311ac0ab4e5f65e02790b3cb4fa15
KevinZhang <zhangkevin34@gmail.com>
2019-10-11 12:49:04 +1100

    boundary

--- assessment4/Assets/C#/BlockMove.cs
+++ assessment4/Assets/C#/BlockMove.cs
@@ -4,30 +4,50 @@ using UnityEngine;
 
 public class BlockMove : MonoBehaviour
 {
+    public static int height = 20;
+    public static int width = 10;
     private float previousTime;
     public float fallingTime = 0.7f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
             transform.position += new Vector3(+1, 0, 0);
         }
-        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
             transform.position += new Vector3(-1, 0, 0);
         }
-        if (Input.GetKeyDown(KeyCode.UpArrow)) {
-            transform.position += new Vector3(0, +1, 0);
-        }
-        if (Time.time - previousTime > (Input.GetKey(KeyCode.UpArrow) ? fallingTime / 10: fallingTime))
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             transform.position += new Vector3(0, +1, 0);
-            previousTime = Time.time; 
+        }
+        if (Time.time - previousTime > (Input.GetKey(KeyCode.UpArrow) ? fallingTime / 10 : fallingTime))
+        {
+            transform.position += new Vector3(0, +1, 0);
+            previousTime = Time.time;
+        }
+
+        bool checkBoundary()
+        {
+            foreach (Transform children in transform)
+            {
+                int roundedX = Mathf.RoundToInt(children.transform.position.x);
+                int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+                if (roundedX < 0 || roundedX >= width || roundedY < 0 || roundedY >= height)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
     }
